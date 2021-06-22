d7cc1efc4caea4107db19e96ca8a41ecbe9e6b15
Shelter Mashonganyika <gigashelter@gmail.com>
2021-06-22 13:02:17 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -7,5 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int)
+int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
