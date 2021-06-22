e1b55037221a158aba1c62b156a49ffeaaf354ee
Shelter Mashonganyika <gigashelter@gmail.com>
2021-06-22 13:09:37 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-  char c[] = "Holberton";
-  int i;
+char c[] = "Holberton";
+int i;
 
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(c[i]);
-    }
-  _putchar('\n');
-  return (0);
+for (i = 0; i < 9; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
